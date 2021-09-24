3193d37a9232e8c15da21bb9709ffa9d55c39684
ujjwal <katanadeveloper01@gmail.com>
2021-09-24 05:38:49 +0530

    add createbank logic

--- app/models.py
+++ app/models.py
@@ -5,3 +5,8 @@ class Wallet(db.Model):
     username = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
     balance = db.Column(db.Integer,nullable=True, default=1000)
+
+class Bank(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    balance = db.Column(db.Integer, default=400000)
+
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from app import app, db
 from flask import render_template
 from werkzeug.utils import redirect
 from flask import request
-from app.models import Wallet
+from app.models import Wallet, Bank
 
 db.create_all()
 
@@ -15,14 +15,18 @@ def addAccount():
     if request.method == 'GET':
         return render_template("signup.html")
     elif request.method == 'POST':
-        try:
-            wallet = Wallet(username=request.form.get("username"), 
-            password=request.form.get("password"))
-            db.session.add(wallet)
-            db.session.commit()
-            return redirect("/accounts")
-        except:
-            return "Try Again"
+        bank = Bank.query.all()
+        if len(bank) >= 1:
+            try:
+                wallet = Wallet(username=request.form.get("username"), 
+                password=request.form.get("password"))
+                db.session.add(wallet)
+                db.session.commit()
+                return redirect("/accounts")
+            except:
+                return "Try Again"
+        else: 
+            return "Ask the admin to create the bank"       
 
 @app.route("/accounts")
 def accounts_page():
@@ -35,5 +39,21 @@ def getWallet(username):
         wallet = Wallet.query.filter_by(username=username).first()
         return render_template("wallet.html", wallet=wallet)
     except:
-        return "User not found"    
+        return "User not found"
+
+
+@app.route('/make/<passcode>')
+def makeBank(passcode):
+    existingBank = Bank.query.all()
+    print(len(existingBank))
+    if len(existingBank) == 1:
+        return "Bank already created"
+    else:
+        if passcode == "makebank":
+            bank = Bank()
+            db.session.add(bank)
+            db.session.commit()
+            return redirect("/")
+        else: return "Wrong password"    
+
             
